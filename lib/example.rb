5b49e619aeb56560942f8b89ad9d8cbecacfedb3
Nicole Gonzalez <gonzalez.s.nicole@gmail.com>
2021-01-16 15:46:58 -0500

    commit tree d8c0b67fb3c59618cc5cb8aa1a0a204369842a14

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,18 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+
+def say_hello_world_ten_times
+    phase = "Hello World!"
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+    puts phase
+end
+say_hello_world_ten_times
